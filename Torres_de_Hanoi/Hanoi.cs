374eec62ea1070bfd9dcdea0615d005d9927b79e
Aitor <aibes1001@gmail.com>
2021-03-28 19:47:09 +0200

    Implementada classe Hanoi i Programa

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -12,11 +12,107 @@ namespace Torres_de_Hanoi
         public void mover_disco(Pila a, Pila b)
         {
 
+            if (a.isEmpty() || (!b.isEmpty() && (b.Top < a.Top)))
+            {
+                Console.WriteLine("Moure disc " + b.Top + " de Torre " + b.Nom + " a Torre " + a.Nom);
+                Disco d = new Disco(b.Top);
+                a.push(d);
+                b.pop();
+            }
+
+            else if (b.isEmpty() || (!a.isEmpty() && (a.Top < b.Top)))
+            {
+                Console.WriteLine("Moure disc " + a.Top + " de Torre " + a.Nom + " a Torre " + b.Nom);
+                Disco d = new Disco(a.Top);
+                b.push(d);
+                a.pop();
+            }
+
         }
 
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
         {
-            return 0;
+            int m = 0;
+
+            if (n % 2 != 0)
+            {
+                do
+                {
+                    m++;
+
+                    if (m == 1 || m % 3 == 1)
+                    {
+                        mover_disco(ini, fin);
+                    }
+                    else if (m == 2 || m % 3 == 2)
+                    {
+                        mover_disco(ini, aux);
+                    }
+                    else if (m == 3 || m % 3 == 0)
+                    {
+                        mover_disco(aux, fin);
+                    }
+
+                    Console.WriteLine("Torre Ini: ");
+                    print_Discos(ini);
+
+                    Console.WriteLine("Torre Aux: ");
+                    print_Discos(aux);
+
+                    Console.WriteLine("Torre Fin: ");
+                    print_Discos(fin);
+
+                    Console.WriteLine("\n");
+
+                } while (fin.Size != n);
+            }
+            else
+            {
+                do
+                {
+                    m++;
+                    if (m == 1 || m % 3 == 1)
+                    {
+                        mover_disco(ini, aux);
+                    }
+                    else if (m == 2 || m % 3 == 2)
+                    {
+                        mover_disco(ini, fin);
+                    }
+                    else if (m == 3 || m % 3 == 0)
+                    {
+                        mover_disco(aux, fin);
+                    }
+
+                    Console.WriteLine("Torre Ini: ");
+                    print_Discos(ini);
+
+                    Console.WriteLine("Torre Aux: ");
+                    print_Discos(aux);
+
+                    Console.WriteLine("Torre Fin: ");
+                    print_Discos(fin);
+
+                    Console.WriteLine("\n");
+
+                } while (fin.Size != n);
+            }
+            return m;
+        }
+
+        public void print_Discos(Pila a)
+        {
+            if (a.Size == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            foreach (Disco d in a.Elementos)
+            {
+                Console.WriteLine(d.Valor);
+            }
+
         }
 
     }
